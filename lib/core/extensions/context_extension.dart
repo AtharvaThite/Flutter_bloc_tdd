ad8bad80fed1d8e16ffdbb4ae7ec8e0b8b5fe788
AtharvaThite <authite@gmail.com>
2024-04-26 11:33:18 +0530

    Added extension to provide user

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_tdd/core/common/app/providers/user_provider.dart';
+import 'package:flutter_bloc_tdd/src/auth/domain/entities/user.dart';
+import 'package:provider/provider.dart';
 
 extension ContextExt on BuildContext {
   //Context extension
@@ -9,4 +12,7 @@ extension ContextExt on BuildContext {
   Size get size => mediaQuery.size;
   double get width => size.width;
   double get height => size.height;
+
+  UserProvider get userProvider => read<UserProvider>();
+  LocalUser? get currentuser => userProvider.user;
 }
