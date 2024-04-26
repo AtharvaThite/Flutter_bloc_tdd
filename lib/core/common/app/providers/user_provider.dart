e271c8570ce1b15635ae1d11d4afe4539a21eba0
AtharvaThite <authite@gmail.com>
2024-04-26 11:38:32 +0530

    User provider

---
+++ lib/core/common/app/providers/user_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc_tdd/src/auth/data/models/user_model.dart';
+
+class UserProvider extends ChangeNotifier {
+  LocalUserModel? _user;
+  LocalUserModel? get user => _user;
+  void initUser(LocalUserModel? user) {
+    if (_user != user) _user = user;
+  }
+
+  set user(LocalUserModel? user) {
+    if (_user != user) {
+      _user = user;
+      Future.delayed(Duration.zero, notifyListeners);
+    }
+  }
+}
