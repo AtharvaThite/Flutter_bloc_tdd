63e8faf71794eaa3005c24cd65c2c10fcdcc6c6f
AtharvaThite <authite@gmail.com>
2024-04-26 11:30:25 +0530

    Implemented FIrebase and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_tdd/core/common/app/providers/user_provider.dart';
 import 'package:flutter_bloc_tdd/core/res/colours.dart';
 import 'package:flutter_bloc_tdd/core/res/fonts.dart';
 import 'package:flutter_bloc_tdd/core/service/injection_container.dart';
 import 'package:flutter_bloc_tdd/core/service/router.dart';
 import 'package:flutter_bloc_tdd/firebase_options.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance
+      // Your personal reCaptcha public key goes here:
+      .activate(
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.debug,
+    // webProvider: ReCaptchaV3Provider(kWebRecaptchaSiteKey),
   );
   await init();
   runApp(const MyApp());
@@ -20,21 +31,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Education App',
-      theme: ThemeData(
-        useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: Colours.primaryColor,
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Education App',
+        theme: ThemeData(
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: Colours.primaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.transparent,
+          ),
+          fontFamily: Fonts.poppins,
         ),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-        ),
-        fontFamily: Fonts.poppins,
+        onGenerateRoute: generateRoute,
       ),
-      onGenerateRoute: generateRoute,
     );
   }
 }
